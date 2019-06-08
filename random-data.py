e9659e4f3e23b61d54edbfefac74aca7754ee85e
Túlio Lima <tulio1502@hotmail.com>
2019-06-08 17:27:57 -0300

    final random

--- random-data.py
+++ random-data.py
@@ -92,6 +92,6 @@ for word in RandomDataValidation:
     DataFileValidation.write(str(word)+'\n')
 DataFileValidation.close()
 
-call('nohup ./server.py --batch 50 --port 5557 --validation --list TrainData.txt &',shell=True)
-call('nohup ./server.py --batch 50 --port 5556 --list TestData.txt &', shell=True)
+call('nohup ./server.py --batch 200 --port 5557 --validation --list TrainData.txt &',shell=True)
+call('nohup ./server.py --batch 200 --port 5556 --list TestData.txt &', shell=True)
 
